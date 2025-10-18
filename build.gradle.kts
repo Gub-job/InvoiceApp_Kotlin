066dfd3025975b0e3afaf564c3fbd02a2731b531
Gub-job <2311500003@mahasiswa.atmaluhur.ac.id>
2025-10-18 12:57:40 +0700

    tambah gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,27 +8,19 @@ repositories {
 }
 
 dependencies {
-    // JavaFX
-    implementation("org.openjfx:javafx-controls:21.0.1")
-    implementation("org.openjfx:javafx-fxml:21.0.1")
+    implementation(kotlin("stdlib"))
+}
 
-    // SQLite
-    implementation("org.xerial:sqlite-jdbc:3.45.1.0")
-
-    // PDF Export (OpenPDF)
-    implementation("com.github.librepdf:openpdf:1.3.30")
-
-    // Excel Export (Apache POI)
-    implementation("org.apache.poi:poi:5.2.5")
-    implementation("org.apache.poi:poi-ooxml:5.2.5")
+javafx {
+    version = "17"
+    modules("javafx.controls", "javafx.fxml")
 }
 
 application {
-    mainClass.set("main.MainKt")
+    mainClass.set("MainAppKt")
 
-    // Jalankan JavaFX
     applicationDefaultJvmArgs = listOf(
-        "--module-path", "PATH_JAVAFX_LIB",
+        "--module-path", "D:/JavaFX/javafx-sdk-17/lib",
         "--add-modules", "javafx.controls,javafx.fxml"
     )
 }
