06cb90f7f7ebf78f1aea064998b671055371e5ce
Mario Valentino <2311500003@mahasiswa.atmaluhur.ac.id>
2025-10-22 20:37:46 +0700

    masih set up form pelanggan

--- src/main/kotlin/controller/MainController.kt
+++ src/main/kotlin/controller/MainController.kt
@@ -6,6 +6,9 @@ import javafx.scene.layout.BorderPane
 import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 import javafx.scene.Scene
+import ui.PelangganScreen
+import ui.PerusahaanScreen
+
 
 class MainController {
     @FXML
