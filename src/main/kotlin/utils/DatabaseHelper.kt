9178a338452a99d79c703d875ba666c7a19faffb
Mario Valentino <2311500003@mahasiswa.atmaluhur.ac.id>
2025-10-30 19:13:48 +0700

    Bagusin mainview

--- src/main/kotlin/utils/DatabaseHelper.kt
+++ src/main/kotlin/utils/DatabaseHelper.kt
@@ -3,9 +3,9 @@ package utils
 import java.sql.Connection
 import java.sql.DriverManager
 
-object DatabaseHelper {
+object DatabaseHelper{
     private const val DB_PATH = "src/main/resources/database/invoice_app.db"
-
+            
     fun getConnection(): Connection {
         return DriverManager.getConnection("jdbc:sqlite:$DB_PATH")
     }
